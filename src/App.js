0f38eaa52a1e162ef35386188964cb83071c1dd1
rajeshavul2 <rajeshavul2@gmail.com>
2023-12-04 15:42:13 +0200

    lab done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Toolbar2 from './Toolbar2';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Passing Event Handlers as Props</h1>
+      <Toolbar2 />
+    </div>
+  );
+}
+
+export default App;
